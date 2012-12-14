2c5d50783678b88788b2b844e99d626c35369d65
Daniel Stevens <dan.stevens@oracle.com>
2012-12-14 16:45:36 +0000

    Replaced tabs with space in source code.

--- ieproxy/CommandLineUI.h
+++ ieproxy/CommandLineUI.h
@@ -11,33 +11,33 @@
 namespace ieproxy
 {
 
-	namespace po = boost::program_options;
+    namespace po = boost::program_options;
 
-	class CommandLineUI
-	{
+    class CommandLineUI
+    {
 
-	private:
+    private:
 
-		const char* _ieproxy;
-		std::ostream& _cout;
-		std::ostream& _cerr;
-		po::options_description _optDesc;
-		po::variables_map _optMap;
-		ProxySettings _proxySettings;
+        const char* _ieproxy;
+        std::ostream& _cout;
+        std::ostream& _cerr;
+        po::options_description _optDesc;
+        po::variables_map _optMap;
+        ProxySettings _proxySettings;
 
-		void procOptions(int argc, char* argv[]);
-		void printHelp();
-		void printVersion();
-		void printAutoConfigSettings(const ieproxy::ProxySettings& p);
-		void printProxySettings(const ieproxy::ProxySettings& p);
+        void procOptions(int argc, char* argv[]);
+        void printHelp();
+        void printVersion();
+        void printAutoConfigSettings(const ieproxy::ProxySettings& p);
+        void printProxySettings(const ieproxy::ProxySettings& p);
 
-	public:
-		CommandLineUI(std::ostream& cout, std::ostream& cerr);
-		~CommandLineUI(void);
-	
-		int Run(int argc, char* argv[]);
-		const ProxySettings& GetProxySettings() { return _proxySettings; }
-		
-	};
+    public:
+        CommandLineUI(std::ostream& cout, std::ostream& cerr);
+        ~CommandLineUI(void);
+    
+        int Run(int argc, char* argv[]);
+        const ProxySettings& GetProxySettings() { return _proxySettings; }
+        
+    };
 
 }
